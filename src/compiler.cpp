9a93993155ba8db1ebd18aff78d0d6bd2c0160b3
Alex Ferbrache <Alex.Ferbrache@gmail.com>
2017-07-30 18:27:41 -0700

    Lexing simple funcs

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -1,8 +1,45 @@
 // compiler.cpp : Defines the entry point for the console application.
 //
 
+#include "lexer.h"
+#include <iostream>
+
+std::string test = 
+"                       \
+    function foo(bar) { \
+                        \
+    }                   \
+";
+
+TokenType testArr[] =
+{
+    TokenType::K_FUNCTION,
+    TokenType::IDENT,
+    TokenType::LPAREN,
+    TokenType::IDENT,
+    TokenType::RPAREN,
+    TokenType::LBRACE,
+    TokenType::RBRACE
+};
+
 int main()
 {
+    auto lexer = Lexer(test);
+    auto current = lexer.Current();
+    
+    int i = 0;
+    while (current->Type != TokenType::END)
+    {
+        if(current->Type != testArr[i])
+        {
+            std::cout << "FAIL: " << current->Type << " != " << testArr[i] << std::endl;
+            return -1;
+        }
+        lexer.Advance();
+        i++;
+    }
+
+    std::cout << "SUCCESS!" << std::endl;
     return 0;
 }
 
